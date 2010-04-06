9c71b2b92d4cf6eb28d8cb4d20b9bc600535e7dc
dmitriyev.viktor <dmitriyev.viktor@94a48314-485e-669b-3f3b-c38e8b6970d3>
2010-04-06 07:22:16 +0000

    Adding the icon for the each user tweet

--- trunk/TwitterSwingClient/src/twitterclient/Item.java
+++ trunk/TwitterSwingClient/src/twitterclient/Item.java
@@ -1,10 +1,15 @@
 package twitterclient;
 
 import java.awt.Component;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
 import twitter.whatareyoudoingservice.twitterresponse.StatusType;
-import twitterclient.utils.*;
+import twitter.whatareyoudoingservice.twitterresponse.UserType;
 
 /*
  * To change this template, choose Tools | Templates
@@ -39,20 +44,18 @@ public class Item extends javax.swing.JPanel implements ListCellRenderer {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextPane1 = new javax.swing.JTextPane();
-		jLabel1 = new javax.swing.JLabel();
-		/*
-		try {
-			jLabel1 = new LinkLabel("Default name");
-		} catch(Exception ex) {
-			ex.printStackTrace();
-		}
-		*/
-		//jLabel1 = new LinkLabel("Default name");
+        jLabel1 = new javax.swing.JLabel();
+        jLabelUserIcon = new javax.swing.JLabel();
+
         jScrollPane1.setViewportView(jTextPane1);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(0, 0, 204));
         jLabel1.setText("Username");
+
+        jLabelUserIcon.setText("Icon");
+        jLabelUserIcon.setMaximumSize(new java.awt.Dimension(48, 48));
+        jLabelUserIcon.setMinimumSize(new java.awt.Dimension(48, 48));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -61,33 +64,48 @@ public class Item extends javax.swing.JPanel implements ListCellRenderer {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 330, Short.MAX_VALUE))
-                .addContainerGap())
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabelUserIcon, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 316, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 60, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 60, Short.MAX_VALUE)
+                    .addComponent(jLabelUserIcon, javax.swing.GroupLayout.DEFAULT_SIZE, 60, Short.MAX_VALUE))
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    //private LinkLabel jLabel1;
-	private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabelUserIcon;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextPane jTextPane1;
     // End of variables declaration//GEN-END:variables
 
     public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-        StatusType st = (StatusType) value;
-        jTextPane1.setText(st.getText());
-        jLabel1.setText("<html>" + st.getUser().getScreenName() + "</html>");
+
+        try {
+            StatusType st = (StatusType) value;
+            jTextPane1.setText(st.getText());
+            jLabel1.setText("<html>" + st.getUser().getScreenName() + "</html>");            
+            UserType user = st.getUser();
+            String iconSrc = user.getProfileImageUrl();
+            URL iconUrl = new URL(iconSrc);
+            ImageIcon icon = new ImageIcon(iconUrl, user.getScreenName());
+            jLabelUserIcon.setIcon(icon);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(Item.class.getName()).log(Level.SEVERE, null, ex);
+        }
         return this;
     }
 
